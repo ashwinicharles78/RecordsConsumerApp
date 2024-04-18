826572273ca558d23a5f760feb41a756471df172
Ashwini Charles <ashwini.charles@enterprisewide.com>
2024-04-19 00:52:05 +0530

    added Kafka endpoint

--- src/main/java/com/records/Records/config/KafkaConsumerConfig.java
+++ src/main/java/com/records/Records/config/KafkaConsumerConfig.java
@@ -2,8 +2,10 @@ package com.records.Records.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.KafkaListenerContainerFactory;
@@ -19,11 +21,16 @@ import java.util.Map;
 @EnableKafka
 @Configuration
 public class KafkaConsumerConfig {
+    private static final String KAFKA_ENDPOINT = "kafka.server.endpoint";
+
+    @Autowired
+    private Environment env;
+
     @Bean
     public Map<String, Object> consumerConfig() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "localhost:9092");
+                env.getProperty(KAFKA_ENDPOINT));
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
                 StringDeserializer.class);
         props.put(ConsumerConfig.GROUP_ID_CONFIG,
